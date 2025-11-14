d67708fea6bbf7802f79a21dbfa619c545e65f5b
lsmt-uol <leedssustainablemarineteam@gmail.com>
2025-11-14 18:02:10 +0000

    Header Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./components/layout/Navbar";
 
 import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 import Firestore from "./components/Firestore";
 import Firestorage from "./components/Firestorage";
 
@@ -8,9 +8,9 @@ export default function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
       <>
-      <Navbar />
-      <Firestore message ="team_members"/>
-      <Firestorage />
+      <Header />
+      {/* <Firestore message ="team_members"/>
+      <Firestorage />*/}
       <Footer />
       </>
     </div>
