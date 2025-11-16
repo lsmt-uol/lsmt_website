76d4f50fa91c2d0b1f1845adc91336ad847fcf08
lsmt-uol <LeedsSustainableMarineTeam@gmail.com>
2025-11-16 21:28:11 +0000

    About Us

--- src/components/layout/AboutUs.jsx
+++ src/components/layout/AboutUs.jsx
@@ -16,31 +16,24 @@ export default function Aboutus() {
     }, []);
 
     return (
-        // flex-col for mobile (stacks in code order: Image then Text)
-        // md:flex-row-reverse for desktop (reverses code order: Text then Image)
-        // md:items-stretch ensures both columns are the same height
         <section className="w-full bg-black h-fit py-3 flex flex-wrap md:flex-row-reverse flex-col md:items-stretch"> 
-
-            {/* 1. IMAGE CONTAINER (Right on Desktop, Top on Mobile) */}
             <div className="md:w-1/2 w-full h-full ">
                 <img
                     src={aboutImage}
                     alt="Leeds Sustainable Marine Team"
-                    // w-full h-auto makes the image shrink on small screens
                     className="w-full h-auto object-cover md:h-full" 
                 />
             </div>
 
-            {/* 2. TEXT CONTAINER (Left on Desktop, Bottom on Mobile) */}
             <div className="md:w-1/2 w-full h-full pt-8 px-6 pb-1 overflow-hidden flex flex-col">
                 
                 <h1 className="text-white font-semibold text-2xl text-center">ABOUT US</h1>
                 <div className="bg-blue-500 w-full h-0.5 my-2"></div>
                 
-                {/* Wrapper to force vertical growth of the text content */}
+                
                 <div className="flex flex-col justify-between flex-grow">
                     
-                    {/* The paragraphs are grouped to ensure the available space is distributed */}
+               
                     <p className="text-white m-0 mt-3 leading-relaxed text-justify">
                         <span className="text-amber-400 font-[5000]">Leeds Sustainable Marine Team (LSMT)</span> is a multidisciplinary team of engineering students from the University of Leeds that focuses on furthering sustainable marine technology through innovative design and participation in the Monaco Energy Boat Challenge (MEBC). We develop environmentally responsible propulsion systems, including hydrogen and electric power, to drive the future of clean marine transport. Our team participates in international marine engineering challenges that foster technical excellence, sustainability, and leadership. LSMT is committed to pushing the boundaries of sustainable marine innovation while preparing future engineers for impactful careers.
                     </p>
